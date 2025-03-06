7a9dbd299cb247d743914aef6fe7b75be6683a01
HyperVaz <timvas14@gmail.com>
2025-03-06 18:30:11 +0300

    baracuda

--- src/build/PartSelector.vue
+++ src/build/PartSelector.vue
@@ -1,16 +1,15 @@
 <template>
-  <div class="part">
-    <img :src="selectedPart.src" title="arm"/>
+  <div class="part" :class="position">
+    <img :src="selectedPart.src" @click="showPartInfo = !showPartInfo" title="arm"/>
     <button @click="selectPreviousPart()" class="prev-selector"></button>
     <button @click="selectNextPart()" class="next-selector"></button>
     <span class="sale" v-show="selectedPart.onSale">Sale!</span>
   </div>
+
+
 </template>
 
 <script>
-import availableParts from '../data/parts';
-
-const parts = availableParts.heads;
 
 function getPreviousValidIndex(index, length) {
   const deprecatedIndex = index - 1;
@@ -23,25 +22,41 @@ function getNextValidIndex(index, length) {
 }
 
 export default {
+  props: {
+    parts: {type: Array, required: true}, position: {
+      type: String, required: true, validator(value) {
+        return ['left', 'right', 'top', 'bottom', 'center'].includes(value);
+      },
+    }
+  },
   data() {
-    return { selectedPartIndex: 0 };
+    return {selectedPartIndex: 0, showPartInfo: false};
   },
   computed: {
     selectedPart() {
-      return parts[this.selectedPartIndex];
+      return this.parts[this.selectedPartIndex];
     },
   },
+  created() {
+    this.emitSelectedPart()
+  },
+  updated() {
+    this.emitSelectedPart()
+  },
   methods: {
+    emitSelectedPart() {
+      this.$emit('partSelected', this.selectedPart);
+    },
     selectNextPart() {
       this.selectedPartIndex = getNextValidIndex(
         this.selectedPartIndex,
-        parts.length,
+        this.parts.length,
       );
     },
     selectPreviousPart() {
       this.selectedPartIndex = getPreviousValidIndex(
         this.selectedPartIndex,
-        parts.length,
+        this.parts.length,
       );
     },
 
@@ -51,12 +66,14 @@ export default {
 </script>
 
 <style scoped>
+
 .part {
   position: relative;
-  width:165px;
-  height:165px;
+  width: 165px;
+  height: 165px;
   border: 3px solid #aaa;
 }
+
 .sale {
   position: absolute;
   bottom: 5px;
@@ -65,6 +82,7 @@ export default {
   background-color: red;
   padding: 3px;
 }
+
 .part-title {
   position: absolute;
   width: 100%;
@@ -74,64 +92,80 @@ export default {
   padding-top: 5px;
   top: -25px;
 }
+
 .part img {
-  width:165px;
+  width: 165px;
 }
+
 .top {
   border-bottom: none;
 }
+
 .left {
   border-right: none;
 }
+
 .right {
   border-left: none;
 }
+
 .bottom {
   border-top: none;
 }
+
 .left img {
   transform: rotate(-90deg);
 }
+
 .right img {
   transform: rotate(90deg);
 }
+
 .prev-selector {
   position: absolute;
-  z-index:1;
+  z-index: 1;
   top: -3px;
   left: -28px;
   width: 25px;
   height: 171px;
 }
+
 .next-selector {
   position: absolute;
-  z-index:1;
+  z-index: 1;
   top: -3px;
   right: -28px;
   width: 25px;
   height: 171px;
 }
-.left .prev-selector:after,  .right .prev-selector:after{
+
+.left .prev-selector:after, .right .prev-selector:after {
   content: '\25B2'
 }
+
 .left .next-selector:after, .right .next-selector:after {
   content: '\25BC'
 }
-.top .prev-selector:after, .bottom .prev-selector:after, .center .prev-selector:after{
+
+.top .prev-selector:after, .bottom .prev-selector:after, .center .prev-selector:after {
   content: '\25C4'
 }
-.top .next-selector:after, .bottom .next-selector:after, .center .next-selector:after{
+
+.top .next-selector:after, .bottom .next-selector:after, .center .next-selector:after {
   content: '\25BA'
 }
+
 .center .prev-selector, .center .next-selector {
-  opacity:0.8;
+  opacity: 0.8;
 }
+
 .left .prev-selector {
   top: -28px;
   left: -3px;
   width: 144px;
   height: 25px;
 }
+
 .left .next-selector {
   top: auto;
   bottom: -28px;
@@ -139,12 +173,14 @@ export default {
   width: 144px;
   height: 25px;
 }
+
 .right .prev-selector {
   top: -28px;
   left: 24px;
   width: 144px;
   height: 25px;
 }
+
 .right .next-selector {
   top: auto;
   bottom: -28px;
@@ -152,6 +188,7 @@ export default {
   width: 144px;
   height: 25px;
 }
+
 .highlight {
   border: 1px solid red;
 }
--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -1,22 +1,55 @@
 <template>
   <div class="content">
-    <button class="add-to-cart" @click="addToCart()">Add to cart!</button>
+    <div class="preview">
+      <CollapsibleSection>
+        <div class="preview-content">
+          <div class="top-row">
+            <img :src="selectedRobot.head.src"/>
+          </div>
+          <div class="middle-row">
+            <img :src="selectedRobot.leftArm.src" class="rotate-left"/>
+            <img :src="selectedRobot.torso.src"/>
+            <img :src="selectedRobot.rightArm.src" class="rotate-right"/>
+          </div>
+
+          <div class="bottom-row">
+            <img :src="selectedRobot.base.src"/>
+          </div>
+        </div>
+      </CollapsibleSection>
+      <button class="add-to-cart" @click="addToCart()">Add to cart!</button>
+    </div>
+
     <div class="top-row">
       <div class="top part" :class="[saleBorderClass, 'top', 'part']">
-<!--        <div class="robot-name">-->
-<!--          {{ selectedRobot.head.title }}-->
-<!--          <span v-if="selectedRobot.head.onSale" class="sale">Sale!</span>-->
-<!--        </div>-->
-        <PartSelector/>
+        <!--        <div class="robot-name">-->
+        <!--          {{selectedRobot.head.title}}-->
+        <!--          <span v-if="selectedRobot.head.onSale" class="sale">Sale!</span>-->
+        <!--        </div>-->
+        <PartSelector
+          :parts="availableParts.heads"
+          position="top"
+          @partSelected="part => selectedRobot.head = part"
+        />
       </div>
     </div>
     <div class="middle-row">
-      <PartSelector/>
-      <PartSelector/>
-      <PartSelector/>
+      <PartSelector
+        :parts="availableParts.arms"
+        position="left"
+        @partSelected="part => selectedRobot.leftArm = part"
+      />
+      <PartSelector
+        :parts="availableParts.torsos"
+        position="center"
+        @partSelected="part => selectedRobot.torso = part"
+      />
+      <PartSelector :parts="availableParts.arms" position="right"
+                    @partSelected="part => selectedRobot.rightArm= part"/>
     </div>
     <div class="bottom-row">
-      <PartSelector/>
+      <PartSelector :parts="availableParts.bases" position="bottom"
+                    @partSelected="part => selectedRobot.base = part"/>
     </div>
     <div>
       <h1>Cart</h1>
@@ -44,26 +77,27 @@
 <script>
 import availableParts from '../data/parts'
 import PartSelector from "@/build/PartSelector.vue";
+import CollapsibleSection from "@/shared/CollapsibleSection.vue";
 
 
 export default {
   name: 'RobotBuilder',
-  components: {PartSelector},
-  data () {
+  components: {CollapsibleSection, PartSelector},
+  data() {
     return {
       availableParts,
       cart: [],
-      selectedRobot:{
-          head: {},
-          leftArm: {},
-          torso:{},
-          rightArm: {},
-          base: {},
+      selectedRobot: {
+        head: {},
+        leftArm: {},
+        torso: {},
+        rightArm: {},
+        base: {},
       },
     }
   },
   computed: {
-    saleBorderClass (){
+    saleBorderClass() {
       return this.selectedRobot.head.onSale ? 'sale-border' : '';
     },
     headBorderStyle() {
@@ -74,7 +108,7 @@ export default {
 
   },
 
-  methods:{
+  methods: {
     addToCart() {
       const robot = this.selectedRobot;
       const cost = robot.head.cost + robot.leftArm.cost + robot.torso.cost + robot.rightArm.cost
@@ -88,103 +122,159 @@ export default {
 </script>
 
 <style scoped>
-.sale-border{
+.part-info {
+  position: absolute;
+  top: -20px;
+  left: 0;
+  width: 210px;
+  height: 210px;
+  padding: 5px;
+}
+.preview {
+  position: absolute;
+  top: -20px;
+  right: 0;
+  width: 210px;
+  height: 210px;
+  padding: 5px;
+}
+
+.preview-content {
+  border: 1px solid #999;
+}
+
+.preview img {
+  width: 50px;
+  height: 50px;
+}
+
+.rotate-right {
+  transform: rotate(90deg);
+}
+
+.rotate-left {
+  transform: rotate(-90deg);
+}
+
+.sale-border {
   border: 3px solid red;
 }
+
 td, th {
   text-align: left;
   padding: 5px;
   padding-right: 20px;
 }
-.cost{
+
+.cost {
   text-align: right;
 }
-.content{
+
+.content {
   position: relative;
 }
+
 .add-to-cart {
   position: absolute;
   right: 30px;
-  width: 220px;
+  width: 210px;
   padding: 3px;
   font-size: 16px;
 }
-.sale{
+
+.sale {
   color: red;
 }
+
 .robot-name {
   position: absolute;
   top: -25px;
   text-align: center;
   width: 100%;
 }
+
 .part {
   position: relative;
-  width:165px;
-  height:165px;
+  width: 165px;
+  height: 165px;
   border: 3px solid #aaa;
 }
-.part{
+
+.part {
   img {
-    width:165px;
+    width: 165px;
   }
 }
+
 .top-row {
-  display:flex;
+  display: flex;
   justify-content: space-around;
 }
+
 .middle-row {
-  display:flex;
+  display: flex;
   justify-content: center;
 }
+
 .bottom-row {
-  display:flex;
+  display: flex;
   justify-content: space-around;
   border-top: none;
 }
+
 .head {
   border-bottom: none;
 }
+
 .left {
   border-right: none;
 }
+
 .right {
   border-left: none;
 }
+
 .left img {
   transform: rotate(-90deg);
 }
+
 .right img {
   transform: rotate(90deg);
 }
+
 .bottom {
   border-top: none;
 }
+
 .prev-selector {
   position: absolute;
-  z-index:1;
+  z-index: 1;
   top: -3px;
   left: -28px;
   width: 25px;
   height: 171px;
 }
+
 .next-selector {
   position: absolute;
-  z-index:1;
+  z-index: 1;
   top: -3px;
   right: -28px;
   width: 25px;
   height: 171px;
 }
+
 .center .prev-selector, .center .next-selector {
-  opacity:0.8;
+  opacity: 0.8;
 }
+
 .left .prev-selector {
   top: -28px;
   left: -3px;
   width: 144px;
   height: 25px;
 }
+
 .left .next-selector {
   top: auto;
   bottom: -28px;
@@ -192,12 +282,14 @@ td, th {
   width: 144px;
   height: 25px;
 }
+
 .right .prev-selector {
   top: -28px;
   left: 24px;
   width: 144px;
   height: 25px;
 }
+
 .right .next-selector {
   top: auto;
   bottom: -28px;
@@ -205,6 +297,7 @@ td, th {
   width: 144px;
   height: 25px;
 }
+
 .right .next-selector {
   right: -3px;
 }
